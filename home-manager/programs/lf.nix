0b03bbf63325e5175486cc18688f16b4c251d4fb
Bartłomiej Broniecki <lomev@disroot.org>
2024-02-16 20:30:50 +0100

    update lf config

--- home-manager/programs/lf.nix
+++ home-manager/programs/lf.nix
@@ -1,11 +1,94 @@
 { pkgs, ... }:
 { 
-  programs.lf.enable = true;
-  programs.lf.settings = {
-    get-mime-type = "%xdg-mime query filetype \"$f\"";
-    open = "$$OPENER $f";
-  };
+  programs.lf = { 
+    enable = true;
 
-  programs.lf.previewer.source = "/home/bart/scope";
+    settings = {
+      shell = "zsh";
+      #shellopts = "-eu";
+      #ifs = "\n";
+      #scrolloff = 10;
+      #number = true;
+    };
+
+    commands = {
+      get-mime-type = "%xdg-mime query filetype \"$f\"";
+      delete = ''
+        set -f
+        printf "$fx\n"
+        printf "delete?[y/n]"
+        read ans
+        [ $ans = "y" ] && rm -rf $fx
+      '';
+
+      extract = ''
+        set -f
+        case $f in
+          *.tar.bz|*.tar.bz2|*.tbz|*.tbz2) tar xjvf $f;;
+          *.tar.gz|*.tgz) tar xzvf $f;;
+          *.tar.xz|*.txz) tar xJvf $f;;
+          *.zip) unzip $f;;
+          *.rar) unrar x $f;;
+          *.7z) 7z x $f;;
+        esac
+      '';
+
+      dragon = "set -f; dragon $fx";
+      tar = ''
+        set -f
+        mkdir $1
+        cp -r $fx $1
+        tar czf $1.tar.gz $1
+        rm -rf $1
+      '';
+
+      zip = ''
+        set -f
+        mkdir $1
+        cp -r $fx $1
+        zip -r $1.zip $1
+        rm -rf $1
+      ''; 
+
+      open = ''
+        case $(file --mime-type "$(readlink -f $f)" -b) in
+	      image/vnd.djvu|application/pdf|application/octet-stream|application/postscript) setsid -f zathura $fx >/dev/null 2>&1 ;;
+          text/*) $EDITOR $fx;;
+	      image/x-xcf) setsid -f gimp $f >/dev/null 2>&1 ;;
+	      image/svg+xml) display -- $f ;;
+	      image/*) sxiv $fx >/dev/null 2>&1 ;;
+	      audio/*) mpv --audio-display=no $f ;;
+	      video/*) setsid -f mpv $f -quiet >/dev/null 2>&1 ;;
+	      application/pdf|application/vnd*|application/epub*) setsid -f zathura $fx >/dev/null 2>&1 ;;
+	      application/pgp-encrypted) $EDITOR $fx ;;
+            *) for f in $fx; do setsid -f $OPENER $f >/dev/null 2>&1; done;;
+        esac
+        }}
+      '';
+    };
+
+    keybindings = {
+      h = "left";
+      n = "down";
+      e = "up";
+      i = "right";
+      #h = "updir";
+      k = "search-next";
+      K = "search-prev";
+      x = "$$f";
+      X = "!$f";
+      #o = "$mimeopen";
+      #O = "$mimeopen --ask $f";
+      #"<enter>" = shell;
+    };
+
+    cmdKeybindings = {
+        
+    };
+
+    previewer.source = "/home/bart/scope";
+    previewer.keybinding = "i";
+    #source = "/home/bart/cleaner";
+  };
 }
 
